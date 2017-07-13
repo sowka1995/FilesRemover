1467363f32571fdbd83c6532ebcc3d1ccbe49fc5
Bartosz Sowa <b.sowa9@gmail.com>
2017-07-13 17:13:00 +0200

    Dodanie klasy FilesRemover obsługjącą logikę aplikacji

--- FilesRemover/FilesRemover.cs
+++ FilesRemover/FilesRemover.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CodeProject;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -9,24 +11,199 @@ namespace FilesRemover
 {
     internal class FilesRemover
     {
-        FilesRemoverForm form;
+        private FilesRemoverModel filesRemoverModel;
+        private Form filesRemoverForm;
 
-        public FilesRemover(FilesRemoverForm form)
+        private IEnumerable<FileData> allFiles;
+        private IEnumerable<string> allDirectories;
+
+        private int allDirectoriesCount;
+        private int allFilesCount;
+        private int maxValueProgressBar;
+
+        ILogger logger;
+        RichTextBox logBox;
+        ProgressBar progressBar;
+
+        public FilesRemover(FilesRemoverModel filesRemoverModel, ILogger logger, RichTextBox logBox, ProgressBar progressBar, Form filesRemoverForm)
         {
-            this.form = form;
+            this.filesRemoverModel = filesRemoverModel;
+            this.filesRemoverForm = filesRemoverForm;
+            this.logger = logger;
+            this.logBox = logBox;
+            this.progressBar = progressBar;
         }
 
-        //private void ChangeEnableControls(bool value)
-        //{
-        //    form.startButton.Enabled = value;
-        //    deleteCopyFilesCheckBox.Enabled = value;
-        //    deleteDirectoriesCheckBox.Enabled = value;
-        //    numberOfWeeks.Enabled = value;
-        //    sourcePathTextBox.Enabled = value;
-        //    destinationPathTextBox.Enabled = value;
-        //    changePaddingButton.Enabled = value;
-        //    paddingValue.Enabled = value;
-        //    overrideFilesCheckBox.Enabled = value;
-        //}
+        public void Start(Action callback)
+        {
+            logBox.Font = new System.Drawing.Font("Arial", 30);
+            logBox.Text = "Inicjalizacja...";
+
+            var jobsWorker = new System.ComponentModel.BackgroundWorker();
+            jobsWorker.DoWork += (ss, ee) => DoJobs();
+            jobsWorker.RunWorkerCompleted += (ss, ee) => { SaveLogToFile(); callback(); };
+
+            var fileInfoWorker = new System.ComponentModel.BackgroundWorker();
+            fileInfoWorker.DoWork += (ss, ee) => { CollectInfoForJobs(); };
+            fileInfoWorker.RunWorkerCompleted += (ss, ee) =>
+            {
+                progressBar.Maximum = maxValueProgressBar;
+                logBox.ResetText(); logBox.Font = new System.Drawing.Font("Arial", 7);
+                jobsWorker.RunWorkerAsync();
+            };
+
+            fileInfoWorker.RunWorkerAsync();
+        }
+
+        private void DoJobs()
+        {
+            if (allFiles != null)
+                CopyAndRemoveUnusedFiles();
+
+            if (allDirectories != null)
+                DeleteEmptyDirectories();
+        }
+
+        private void CollectFilesInfo(string path)
+        {
+            try
+            {
+                allFiles = FastDirectoryEnumerator.EnumerateFiles(path, "*", SearchOption.AllDirectories);
+                allFilesCount = allFiles.Count();
+                maxValueProgressBar += allFilesCount;
+            }
+            catch (Exception ex)
+            {
+                logger.Log(ex.Message);
+                return;
+            }
+        }
+
+        private void CollectDirectoriesInfo(string path)
+        {
+            try
+            {
+                allDirectories = Directory.EnumerateDirectories(path, "*", SearchOption.AllDirectories);
+                allDirectoriesCount = allDirectories.Count();
+                maxValueProgressBar += allDirectoriesCount;
+            }
+            catch (Exception ex)
+            {
+                logger.Log(ex.Message);
+                return;
+            }
+        }
+
+        private void CopyAndRemoveUnusedFiles()
+        {
+            logBox.Invoke(new MethodInvoker(delegate {
+                logBox.AppendText($"Liczba wszystkich plików: {allFilesCount} \n\n");
+                logBox.AppendText("Pełna scieżka pliku \t Czas ostatniego dostępu do pliku \n");
+            }));
+
+            List<string> errors = new List<string>();
+            int count = 0;
+
+            foreach (FileData file in allFiles)
+            {
+                var lastAccessDate = file.LastAccesTime;
+                if (lastAccessDate <= filesRemoverModel.BorderDate)
+                {
+                    string fileName = Path.GetFileName(file.Path);
+                    try
+                    {
+                        string copiedFilePath = $@"{filesRemoverModel.DestinationPath}\{fileName}";
+                        if (File.Exists(copiedFilePath))
+                        {
+                            copiedFilePath = copiedFilePath.Insert(copiedFilePath.LastIndexOf('.'), "_copy");
+                        }
+                        File.Copy(file.Path, copiedFilePath, filesRemoverModel.OverrideFiles);
+                        File.SetLastAccessTime(copiedFilePath, lastAccessDate);
+                        File.Delete(file.Path);
+
+                        logBox.Invoke(new MethodInvoker(delegate { logBox.AppendText(file.Path + " \t" + lastAccessDate + "\n"); }));
+
+                        count++;
+                    }
+                    catch (Exception ex)
+                    {
+                        errors.Add(ex.Message);
+                    }
+                }
+
+                Application.DoEvents();
+                progressBar.Invoke(new MethodInvoker(delegate { progressBar.PerformStep(); }));
+            }
+
+            logBox.Invoke(new MethodInvoker(delegate
+            {
+                logBox.Text = $"Liczba skopiowanych plików: {count}\n" + logBox.Text;
+                if (errors.Count != 0)
+                    logBox.AppendText("\n\n " + string.Join("\n", errors.ToArray()));
+            }));
+        }
+
+        private void DeleteEmptyDirectories()
+        {
+            List<string> errors = new List<string>();
+            int count = 0;
+
+            logBox.Invoke(new MethodInvoker(delegate
+            {
+                logBox.AppendText($"Liczba wszystkich katalogów: {allDirectoriesCount} \n");
+            }));
+
+            foreach (string directory in allDirectories)
+            {
+                if (IsDirectoryEmpty(directory))
+                {
+                    try
+                    {
+                        Directory.Delete(directory);
+                        logBox.Invoke(new MethodInvoker(delegate { logBox.AppendText(directory + " \n"); }));
+                        count++;
+                    }
+                    catch (Exception ex)
+                    {
+                        errors.Add(ex.Message);
+                    }
+                }
+                progressBar.Invoke(new MethodInvoker(delegate { progressBar.PerformStep(); }));
+            }
+
+            logBox.Invoke(new MethodInvoker(delegate { logBox.AppendText($"\nLiczba usuniętych katalogów: {count}"); }));
+
+            if (errors.Count != 0)
+                logBox.Invoke(new MethodInvoker(delegate { logBox.AppendText("\nBłędy: \n\n" + string.Join("\n", errors.ToArray())); }));
+        }
+
+        private void CollectInfoForJobs()
+        {
+            if (filesRemoverModel.CopyAndDeleteFiles)
+                CollectFilesInfo(filesRemoverModel.SourcePath);
+            if (filesRemoverModel.DeleteEmptyDirectories)
+                CollectDirectoriesInfo(filesRemoverModel.SourcePath);
+        }
+
+        private bool IsDirectoryEmpty(string path)
+        {
+            return !Directory.EnumerateFileSystemEntries(path).Any();
+        }
+
+        private void SaveLogToFile()
+        {
+            DateTime dateTime = DateTime.Now;
+            string logFileName = dateTime.ToString("yy-MM-dd") + "_godz_" + dateTime.ToString("HH-mm-ss");
+            string path = Directory.GetCurrentDirectory() + $@"\{logFileName}.txt";
+
+            try
+            {
+                logBox.SaveFile(path, RichTextBoxStreamType.PlainText);
+            }
+            catch (Exception ex)
+            {
+                logger.Log(path + "\n" + ex.Message);
+            }
+        }
     }
 }
--- FilesRemover/FilesRemoverForm.cs
+++ FilesRemover/FilesRemoverForm.cs
@@ -14,25 +14,20 @@ namespace FilesRemover
 
         private Stopwatch _stopwatch;
 
-        private IEnumerable<FileData> allFiles;
-        private IEnumerable<string> allDirectories;
-
         private int maxValueProgressBar;
 
-        private int allDirectoriesCount;
-        private int allFilesCount;
-
         private FilesRemoverModel filesRemoverModel;
+        private FilesRemover filesRemover;
 
         public FilesRemoverForm()
         {
             InitializeComponent();
 
-            filesRemoverModel = new FilesRemoverModel();
-
             // Logi do MessageBox-a
             messageBoxLogger = new MessageBoxLogger();
 
+            filesRemoverModel = new FilesRemoverModel();
+     
             // padding okienka wyświetlającego logi
             logBox.SelectionTabs = new[] {0, 800};
         }
@@ -59,28 +54,18 @@ namespace FilesRemover
 
             if (IsOptionsValid())
             {
-                StartTimer();
+                filesRemover = new FilesRemover(filesRemoverModel, messageBoxLogger, logBox, progressBar, this);
+
                 UseWaitCursor = true;
                 Application.DoEvents();
 
-                logBox.Font = new System.Drawing.Font("Arial", 30);
-                logBox.Text = "Inicjalizacja...";
-
-                var jobsWorker = new System.ComponentModel.BackgroundWorker();
-                jobsWorker.DoWork += (ss, ee) => DoJobs();
-                jobsWorker.RunWorkerCompleted += (ss, ee) => { StopTimer(); SaveLogToFile(); ChangeEnableControls(true); allFiles = null; allDirectories = null; };
-
-                var fileInfoWorker = new System.ComponentModel.BackgroundWorker();
-                fileInfoWorker.DoWork += (ss, ee) => { CollectInfoForJobs(); };
-                fileInfoWorker.RunWorkerCompleted += (ss, ee) =>
+                StartTimer();
+                filesRemover.Start(() =>
                 {
-                    progressBar.Maximum = maxValueProgressBar;
+                    StopTimer();
                     UseWaitCursor = false;
-                    logBox.ResetText(); logBox.Font = new System.Drawing.Font("Arial", 7);
-                    jobsWorker.RunWorkerAsync();
-                };
-
-                fileInfoWorker.RunWorkerAsync();
+                    ChangeEnableControls(true);
+                });
             }
             else
                 ChangeEnableControls(true);
@@ -148,7 +133,7 @@ namespace FilesRemover
                 startButton.Enabled = true;
         }
 
-        private void ChangeEnableControls(bool value)
+        public void ChangeEnableControls(bool value)
         {
             startButton.Enabled = value;
             deleteCopyFilesCheckBox.Enabled = value;
@@ -161,138 +146,6 @@ namespace FilesRemover
             overrideFilesCheckBox.Enabled = value;
         }
 
-        private void CollectFilesInfo(string path)
-        {
-            try
-            {
-                allFiles = FastDirectoryEnumerator.EnumerateFiles(path, "*", SearchOption.AllDirectories);
-                allFilesCount = allFiles.Count();
-                maxValueProgressBar += allFilesCount;
-            }
-            catch (Exception ex)
-            {
-                messageBoxLogger.Log(ex.Message);
-                return;
-            }
-        }
-
-        private void CollectDirectoriesInfo(string path)
-        {
-            try
-            {
-                allDirectories = Directory.EnumerateDirectories(path, "*", SearchOption.AllDirectories);
-                allDirectoriesCount = allDirectories.Count();
-                maxValueProgressBar += allDirectoriesCount;
-            }
-            catch (Exception ex)
-            {
-                messageBoxLogger.Log(ex.Message);
-                return;
-            }
-        }
-
-        private void CopyAndRemoveUnusedFiles()
-        {
-            logBox.Invoke(new MethodInvoker(delegate {
-                logBox.AppendText($"Liczba wszystkich plików: {allFilesCount} \n\n");
-                logBox.AppendText("Pełna scieżka pliku \t Czas ostatniego dostępu do pliku \n");
-            }));
-
-            List<string> errors = new List<string>();
-            int count = 0;
-            
-            foreach (FileData file in allFiles)
-            {
-                var lastAccessDate = file.LastAccesTime;
-                if (lastAccessDate <= filesRemoverModel.BorderDate)
-                {
-                    string fileName = Path.GetFileName(file.Path);
-                    try
-                    {
-                        string copiedFilePath = $@"{filesRemoverModel.DestinationPath}\{fileName}";
-                        if (File.Exists(copiedFilePath))
-                        {
-                            copiedFilePath = copiedFilePath.Insert(copiedFilePath.LastIndexOf('.'), "_copy");
-                        }
-                        File.Copy(file.Path, copiedFilePath, filesRemoverModel.OverrideFiles);
-                        File.SetLastAccessTime(copiedFilePath, lastAccessDate);
-                        File.Delete(file.Path);
-
-                        logBox.Invoke(new MethodInvoker(delegate { logBox.AppendText(file.Path + " \t" + lastAccessDate + "\n"); }));
-
-                        count++;
-                    }
-                    catch (Exception ex)
-                    {
-                        errors.Add(ex.Message);
-                    }
-                }
-
-                Application.DoEvents();
-                progressBar.Invoke(new MethodInvoker(delegate { progressBar.PerformStep(); }));
-            }
-
-            logBox.Invoke(new MethodInvoker(delegate
-            {
-                logBox.Text = $"Liczba skopiowanych plików: {count}\n" + logBox.Text;
-                if (errors.Count != 0)
-                    logBox.AppendText("\n\n " + string.Join("\n", errors.ToArray()));
-            }));
-        }
-
-        private void DeleteEmptyDirectories()
-        {
-            List<string> errors = new List<string>();
-            int count = 0;
-
-            logBox.Invoke(new MethodInvoker(delegate
-            {
-                logBox.AppendText($"Liczba wszystkich katalogów: {allDirectoriesCount} \n");
-            }));
-
-            foreach (string directory in allDirectories)
-            {
-                if (IsDirectoryEmpty(directory))
-                {
-                    try
-                    {
-                        Directory.Delete(directory);
-                        logBox.Invoke(new MethodInvoker(delegate { logBox.AppendText(directory + " \n"); }));
-                        count++;
-                    }
-                    catch (Exception ex)
-                    {
-                        errors.Add(ex.Message);
-                    }
-                }
-                progressBar.Invoke(new MethodInvoker(delegate { progressBar.PerformStep(); }));
-            }
-
-            logBox.Invoke(new MethodInvoker(delegate { logBox.AppendText($"\nLiczba usuniętych katalogów: {count}"); }));
-
-            if (errors.Count != 0)
-                logBox.Invoke(new MethodInvoker(delegate { logBox.AppendText("\nBłędy: \n\n" + string.Join("\n", errors.ToArray())); }));
-        }
-
-        private void DoJobs()
-        {      
-            if (allFiles != null)
-                CopyAndRemoveUnusedFiles();
-
-            if (allDirectories != null)
-                DeleteEmptyDirectories();
-
-            progressBar.Invoke(new MethodInvoker(delegate { progressBar.PerformStep(); }));
-        }
-
-        private void CollectInfoForJobs()
-        {
-            if (filesRemoverModel.CopyAndDeleteFiles)
-                CollectFilesInfo(filesRemoverModel.SourcePath);
-            if (filesRemoverModel.DeleteEmptyDirectories)
-                CollectDirectoriesInfo(filesRemoverModel.SourcePath);
-        }
-
         private void InitializeProgressBar()
         {
             maxValueProgressBar = 1;
@@ -301,11 +154,6 @@ namespace FilesRemover
             progressBar.Step = 1;
             progressBar.Maximum = maxValueProgressBar;
             progressBar.ForeColor = System.Drawing.Color.Yellow;
-        }
-
-        private bool IsDirectoryEmpty(string path)
-        {
-            return !Directory.EnumerateFileSystemEntries(path).Any();
         }
 
         private bool IsOptionsValid()
@@ -330,22 +178,6 @@ namespace FilesRemover
             }
 
             return true;
-        }
-
-        private void SaveLogToFile()
-        {
-            DateTime dateTime = DateTime.Now;
-            string logFileName = dateTime.ToString("yy-MM-dd") + "_godz_" + dateTime.ToString("HH-mm-ss");
-            string path = Directory.GetCurrentDirectory() + $@"\{logFileName}.txt";
-
-            try
-            {
-                logBox.SaveFile(path, RichTextBoxStreamType.PlainText);
-            }
-            catch (Exception ex)
-            {
-                messageBoxLogger.Log(path + "\n" + ex.Message);
-            }
         }
 
         private string ShowFolderDialog(string currentPath)
